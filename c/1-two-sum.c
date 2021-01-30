79e3e12f9f689b038bfcbe12f9ead0d379f5b82b
Wenyao <z5253448@ad.unsw.edu.au>
2021-01-31 00:18:45 +1100

    first commit

---
+++ c/1-two-sum.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize);
+
+int main(){
+    int nums[4] = [2,7,11,15];
+    int target = 9
+    int * ans = twoSum(int* nums, int numsSize, int target, int* returnSize);
+    printf("%s", ans);
+    return 0;
+}
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    
+}
