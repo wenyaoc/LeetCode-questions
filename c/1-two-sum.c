cfa048d91420307bca9d727232b37a91784559db
Wenyao Chen <67650151+wenyaoc@users.noreply.github.com>
2021-01-31 15:25:59 +0800

    Update 1-two-sum.c

--- c/1-two-sum.c
+++ c/1-two-sum.c
@@ -1,32 +1,38 @@
-/*
-#include <stdio.h>
-#include <stdlib.h>
-
-
-int* twoSum(int* nums, int numsSize, int target, int* returnSize);
-
-int main(){
-    int nums[3] = {3, 2, 4};
-    int target = 6;
-    int * returnSize = malloc(sizeof(int));
-    int * ans = twoSum(nums, sizeof(nums) / sizeof(int), target, returnSize);
-    printf("%d %d\n", ans[0], ans[1]);
-    return 0;
+#include <math.h>
+struct hash_table_entry {
+	int hash, key, value;
+};
+#define hash_fn(key) (abs(key % htable_size) + 1)
+#define prob_fn(key) (abs(key % htable_size) + 1)
+void  insert_htable(struct hash_table_entry *htable, int htable_size, int key, int value)
+{
+	int hash = hash_fn(key), tmphash = hash;
+	while (htable[tmphash].hash)
+		tmphash = prob_fn(tmphash);
+	htable[tmphash] = (struct hash_table_entry){hash,key,value};
 }
-*/
-int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
-    int * ans = malloc(2 * sizeof(int));
-    for (int i = numsSize - 1; i >= 0; i--) {
-        for (int j = 0; j < i; j++) {
-            int total = nums[i] + nums[j];
-            if (total == target) {
-                ans[0] = j;
-                ans[1] = i;
-                *returnSize = 2;
-                return ans;
-            } 
-        }
-    }
-    *returnSize = 0;
-    return ans;
+int find_htable(struct hash_table_entry *htable, int htable_size, int key, int*found_idx)
+{
+	int hash = hash_fn(key), tmphash = hash;
+	while (htable[tmphash].hash && htable[tmphash].key != key)
+		tmphash = prob_fn(tmphash);
+    *found_idx = htable[tmphash].value;
+	return htable[tmphash].hash == hash;
+}
+int *twoSum(int *nums, int numsSize, int target, int *returnSize)
+{
+	int htable_size = numsSize * 2, *p = malloc(sizeof(int) * 2), found_idx;
+	struct hash_table_entry *htable = calloc(htable_size + 1, sizeof(struct hash_table_entry));
+    *returnSize = 2;
+	for (int i = 0; i < numsSize; ++i) {
+		if (find_htable(htable, htable_size, target - nums[i], &found_idx)) {
+			p[0] = found_idx;
+			p[1] = i;
+			break;
+		} else {
+			insert_htable(htable, htable_size, nums[i], i);
+		}
+	}
+    free(htable);
+    return p;
 }
