f33e4890ea6ca1e88f65936c30eeb4e9e572fd73
Wenyao Chen <67650151+wenyaoc@users.noreply.github.com>
2021-01-31 18:22:27 +0800

    Update 4-median-of-two-sorted-arrays.c

--- c/4-median-of-two-sorted-arrays.c
+++ c/4-median-of-two-sorted-arrays.c
@@ -2,31 +2,37 @@ double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Si
     double ans = 0;
     int i = 0, j = 0, k = 0;
     int totalLen = nums1Size + nums2Size;
-    int * totalNums = malloc(totalLen * sizeof(int));
+    int * midNums = malloc(2 * sizeof(int));
 
-    while (k < totalLen) {
+    while (k < totalLen/2+1) {
+        int nextNum;
         if (i >= nums1Size) {
-            totalNums[k] = nums2[j];
+            nextNum = nums2[j];
             j++;
         } else if (j >= nums2Size) {
-            totalNums[k] = nums1[i];
+            nextNum = nums1[i];
             i++;
         } else if(nums1[i] < nums2[j]) {
-            totalNums[k] = nums1[i];
+            nextNum = nums1[i];
             i++;
         } else {
-            totalNums[k] = nums2[j];
+            nextNum = nums2[j];
             j++;
+        }
+        if (totalLen == 1) midNums[0] = nextNum;
+        else if(k == totalLen/2 - 1) {
+            midNums[0] = nextNum;
+        } else if(k == totalLen/2) {
+            midNums[1] = nextNum;
         }
         k++;
     }
-
-    if (totalLen == 1) ans = totalNums[0];
+    if (totalLen == 1) ans = midNums[0];
     else if(totalLen%2 == 0) {
-        ans = totalNums[totalLen/2 - 1] + totalNums[totalLen/2];
+        ans = midNums[0] + midNums[1];
         ans = (double)ans / 2;
     } else {
-        ans = totalNums[totalLen/2];
+        ans = midNums[1];
     }
     return ans;
 }
