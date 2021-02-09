0636763ce850d3fba142041413e401d42f0c0d0a
Wenyao Chen <67650151+wenyaoc@users.noreply.github.com>
2021-02-10 02:07:03 +0800

    Create 70-climbing-stairs.cpp

---
+++ cpp/1-100/70-climbing-stairs.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n <= 2) return n;
+        int pre2 = 1, pre1 = 2, cur;
+        for (int i = 2; i < n; ++i) {
+            cur = pre1 + pre2;
+            pre2 = pre1;
+            pre1 = cur;
+        }
+        return cur;
+    }
+};
