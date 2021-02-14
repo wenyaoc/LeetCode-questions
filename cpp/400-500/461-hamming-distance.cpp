6917d93f1e955c8c8c92541f5fdfea87412cce81
Wenyao Chen <67650151+wenyaoc@users.noreply.github.com>
2021-02-15 00:34:52 +0800

    Create 461-hamming-distance.cpp

---
+++ cpp/400-500/461-hamming-distance.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int z = x ^ y;
+	    int sum = 0;
+	    while (z){
+		    sum += z & 1;
+		    z = z>>1;
+	    }
+	    return sum;
+    }
+};
